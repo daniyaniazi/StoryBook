69a21f27619f4145f7c2a6998448f4bb6068365c
daniyaniazi <farihadania@hotmail.com>
2021-04-12 00:36:39 +0500

    single story page and user stories page

--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuth } = require('../middleware/auth')
+const story = require('../models/story')
 const Story = require('../models/story')
 //@desc show add page
 //@route GET /stories/add
@@ -44,48 +45,128 @@ router.get('/', ensureAuth, async (req, res,) => {
 //@desc show edit stories page
 //@route GET /stories/edit
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-    const story = await Story.findOne({
-        _id: req.params.id
-    }).lean()
 
-    if (!story) {
-        return res.render('error/404')
+    try {
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+
+        }
+        else {
+            res.render('stories/edit', {
+                story
+            })
+        }
+    } catch (error) {
+        console.log("update error", error)
+        res.render('error/500')
     }
 
-    if (story.user != req.user.id) {
-        res.redirect('/stories')
 
-    }
-    else {
-        res.render('stories/edit', {
-            story
-        })
-    }
 })
 
 
 //@desc Update story
 //@route PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
-    let story = await Story.findById(req.params.id).lean()
 
-    if (!story) {
-        return res.render('error/404')
+    try {
+        let story = await Story.findById(req.params.id).lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+
+        }
+
+        else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            })
+        }
+        res.redirect('/dashboard')
+    } catch (error) {
+        console.log("update error", error)
+        res.render('error/500')
     }
 
-    if (story.user != req.user.id) {
-        res.redirect('/stories')
-
-    }
-
-    else {
-        story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-            new: true,
-            runValidators: true
-        })
-    }
-    res.redirect('/dashboard')
 })
 
+// @desc    Delete story
+// @route   DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            await Story.remove({ _id: req.params.id })
+            res.redirect('/dashboard')
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public',
+        })
+            .populate('user')
+            .lean()
+
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).populate('user').lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user._id != req.user.id && story.status == 'private') {
+            res.render('error/404')
+        } else {
+            res.render('stories/show', {
+                story,
+            })
+        }
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
 
 module.exports = router
