e5fe405154b6d7ce513b7b4a55b69851cc07a20b
daniyaniazi <farihadania@hotmail.com>
2021-04-18 21:00:47 +0500

    update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ connectDB()
 require('./config/passport')(passport)
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 
 //bodyparser
--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://admin:admin@Cluster0.22jeh.mongodb.net/storybooks?retryWrites=true&w=majority', {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useFindAndModify: false,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true,
         })
         console.log(`MongoDB connected ${conn.connection.host}`)
     } catch (err) {
